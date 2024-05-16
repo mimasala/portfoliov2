e66ea3333d59ff124c4d54bb768727916a9945bb
mimasala <mytallmusic@gmail.com>
2024-05-16 16:22:58 +0200

    checkpoint

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,8 @@
 // app/providers.tsx
 'use client'
 
-import theme from '@/theme'
 import { ChakraProvider } from '@chakra-ui/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  return <ChakraProvider>{children}</ChakraProvider>
 }
