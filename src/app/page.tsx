b7ac1e179df745bb1af675c09cf7eb7aeec35094
mimasala <mytallmusic@gmail.com>
2024-05-16 16:49:16 +0200

    added aceternity-ui and tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 import SplitWithImage from '@/components/SplitWithImage'
+import { PinContainer } from '@/components/ui/3d-pin'
 
 export default function Page() {
   return (
     <>
-      <br />
-      <SplitWithImage></SplitWithImage>
-      <br />
+      <br /> <SplitWithImage></SplitWithImage>
       <br />
       <br />
       <br />
@@ -18,6 +17,10 @@ export default function Page() {
       <br />
       <br />
       <br />
+      <br />
+      <PinContainer
+        title="/ui.aceternity.com"
+        href="https://twitter.com/mannupaaji"><div>Bithchass</div></PinContainer>
       <br />
       <br />
       <br />
