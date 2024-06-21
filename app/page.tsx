40c47b871b31ea4ea0a41d6c7d8f6fb04cf5b292
mimasala <mytallmusic@gmail.com>
2024-06-21 19:51:23 +0200

    wow good checkpoint

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { NavBar } from "@/components/NavBar";
 import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import moment from "moment";
+import Link from "next/link";
 
 export default function Home() {
   const yearSince = (): number => {
@@ -11,16 +12,20 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <div className="fixed -z-50">
-        <BackgroundGradientAnimation>
-          <div className="h-screen flex-wrap items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
-            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-              Mischa Strauss
-            </p>
-            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">hi</p>
-          </div>
-        </BackgroundGradientAnimation>
-      </div>
+      <BackgroundGradientAnimation>
+        <div className="absolute z-50 inset-0 flex flex-col items-center justify-center text-white font-bold px-4 text-3xl text-center md:text-4xl lg:text-7xl space-y-4 pointer-events-none">
+          <p className="bg-clip-text drop-shadow-2xl bg-gradient-to-b text-transparent from-white/80 to-white/20">
+            Mischa Strauss
+          </p>
+          <p className="text-sm bg-clip-text drop-shadow-2xl bg-gradient-to-b text-transparent from-gray-300/80 to-gray-300/20 relative">
+            Application developer based in Switzerland.
+            Currently in a four-year apprenticeship at{" "}
+            <span className="pointer-events-auto relative">
+              <Link href="https://www.six-group.com/de/home.html" className="text-blue-700/80 underline">SIX</Link>.
+            </span>
+          </p>
+        </div>
+      </BackgroundGradientAnimation>
     </>
   );
 }
